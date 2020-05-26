53f59b3438c440bb081c8856e3dfa78ccd107b27
郑文龙 <zhengwenlong@jianke.com>
2020-05-26 14:34:44 +0800

    redux简单实现

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+//index.js
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import { Provider } from './react-redux'
+import { createStore } from './store'
+import { reducer } from './reducer'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+ReactDOM.render(   
+    <Provider store={createStore(reducer)}>        
+        <App />    
+    </Provider>,     
+    document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
